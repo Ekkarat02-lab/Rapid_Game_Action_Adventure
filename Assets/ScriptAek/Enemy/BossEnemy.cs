4383a26152177d2a2964e75b2ff0738f557ab6b9
Ekkarat02-lab <ekkarat.thai@bumail.net>
2024-10-18 16:55:55 +0700

    Platformer

--- Assets/ScriptAek/Enemy/BossEnemy.cs
+++ Assets/ScriptAek/Enemy/BossEnemy.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class BossEnemy : EnemyState
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireRate = 1f;
+
+    private GameObject player;
+    private float nextFireTime;
+
+    private void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+        CurrentHealth = maxHP;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (player != null && Vector2.Distance(transform.position, player.transform.position) <= detectionRange)
+        {
+            if (Time.time >= nextFireTime)
+            {
+                Shoot();
+                nextFireTime = Time.time + 1f / fireRate;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Vector2 direction = (player.transform.position - firePoint.position).normalized;
+        bullet.GetComponent<Bullet>().Initialize(direction);
     }
 }
