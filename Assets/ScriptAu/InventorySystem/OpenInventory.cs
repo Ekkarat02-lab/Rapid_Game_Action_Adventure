a22ad7e71cc1c847e217a56c197fb2b828590d40
aukamon <aukamon2000@gmail.com>
2024-10-19 20:48:15 +0700

    open and close inventory "I" button

--- Assets/ScriptAu/InventorySystem/OpenInventory.cs
+++ Assets/ScriptAu/InventorySystem/OpenInventory.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class OpenInventory : MonoBehaviour
 {
+    [SerializeField] private GameObject inventoryUi;
+    private bool openInventory;
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryUi.SetActive(false);
+        openInventory = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            openInventory = !openInventory;
+            inventoryUi.SetActive(openInventory);
+        }
         
     }
 }
